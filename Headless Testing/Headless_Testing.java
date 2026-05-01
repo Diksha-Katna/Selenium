21a4bd48775adaed466336c56c81dd94f913f746
Diksha-Katna <dikshakatnagenuine@gmail.com>
2026-05-01 22:02:31 +0530

    Headless Testing And Screenshots

---
+++ Headless Testing/Headless_Testing.java
@@ -0,0 +1,33 @@
+package SeleniumLearningSkills;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Headless_Testing {
+
+	public static void main(String[] args) {
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--headless=new");  // setting for headless mode of execution
+		
+			
+		WebDriver driver=new ChromeDriver(options); 
+			       
+		driver.get("https://demo.opencart.com/");
+				
+		String act_title=driver.getTitle();
+		
+		if(act_title.equals("Your Store"))
+		{
+			System.out.println("Test Passed");
+		}
+		else
+		{
+			System.out.println("Test Failed");
+		}
+	
+		driver.quit();
+		
+	}
+
+}
