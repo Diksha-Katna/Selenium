07fecee7e5636f9a8c5ce795a35123c0edf6be5c
Diksha-Katna <dikshakatnagenuine@gmail.com>
2026-05-04 22:24:01 +0530

    Class Annotation

---
+++ TestNG/Annotations/ClassAnnotation.java
@@ -0,0 +1,34 @@
+package SeleniumLearningSkills;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ClassAnnotation {
+	@BeforeClass
+	void login()
+	{
+		System.out.println("This is login..");
+	}
+	
+	@AfterClass
+	void logout()
+	{
+		System.out.println("this is logout...");
+	}
+	
+	@Test(priority=1)
+	void search()
+	{
+		System.out.println("this is search...");
+	}
+	
+	
+	@Test(priority=2)
+	void advacedsearch()
+	{
+		System.out.println("this advanced search....");
+	}
+	
+
+}
